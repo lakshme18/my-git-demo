85eca4b4263022634008aa621c4a6a0acfdb923e
LAKSHMI KUMARI <56174686+lakshme18@users.noreply.github.com>
2022-05-30 11:45:32 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.cts;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Welcome the session");
-		System.out.println("Hi, hope you are fine");
-	}
-
-}
